69a15a308555ce7138efda70311ce411a51a9829
zenglen42 <gsnospampls@pobox.com>
2016-11-14 12:17:12 -0600

    update query for start/stop year

--- warmup/controller.js
+++ warmup/controller.js
@@ -2,14 +2,18 @@
 //
 // https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=0f04f96b81ec4a7d92d9e017b07261b0&q=javascript
 
-console.log("hello");
 var searchTerm = "enigma";
+var startYear =  "1945";	// begin_date = YYYYMMDD
+var stopYear = "1952";		// end_date = YYYYMMDD
+var numRecords = 5;
 
 function searchCallback() {
 	// Built by LucyBot. www.lucybot.com
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	url += '?' + $.param({
 					'api-key': "0f04f96b81ec4a7d92d9e017b07261b0",
+					'begin_date': startYear + "0101",
+					'end_date': stopYear + "0101",
 					'q': searchTerm
 						});
 	$.ajax({
@@ -21,6 +25,11 @@ function searchCallback() {
 	}).fail(function(err) {
 		throw err;
 	});
+}
+
+function clearCallback() {
+	// This function should clear out the html elements that
+	// have presentation data.
 }
 
 searchCallback();
