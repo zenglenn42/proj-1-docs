a466e8b9ce430dbac7388b157c687f9fee1c4143
zenglen42 <gsnospampls@pobox.com>
2016-11-14 13:16:13 -0600

    wire frontend and backend together

--- warmup/controller.js
+++ warmup/controller.js
@@ -2,12 +2,25 @@
 //
 // https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=0f04f96b81ec4a7d92d9e017b07261b0&q=javascript
 
-var searchTerm = "enigma";
-var startYear =  "1945";	// begin_date = YYYYMMDD
-var endYear = "1952";		// end_date = YYYYMMDD
-var numRecords = 5;
+
+
+$("#runSearch").on("click", searchCallback);
 
 function searchCallback() {
+
+/*
+	var searchTerm = "enigma";
+	var startYear =  "1945";	// begin_date = YYYYMMDD
+	var endYear = "1952";		// end_date = YYYYMMDD
+	var numRecords = 5;
+*/
+	searchTerm = $("#searchTerm").val();
+	startYear = $("#startYear").val();
+	endYear = $("#endYear").val();
+	numRecords = $("numRecords").val();
+
+	console.log(searchTerm);
+
 	// Built by LucyBot. www.lucybot.com
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	url += '?' + $.param({
@@ -21,10 +34,12 @@ function searchCallback() {
 		method: 'GET',
 	}).done(function(result) {
 		// This is where we parse down the search results.
+		$("#searchResults").text(JSON.stringify(result));
 		console.log(result);
 	}).fail(function(err) {
 		throw err;
 	});
+	return false;
 }
 
 function clearCallback() {
